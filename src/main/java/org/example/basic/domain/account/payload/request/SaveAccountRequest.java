39c8a2e739b029a9b1998a0a9ab2c7145baa2472
namgyunkim <skarbs01@naver.com>
2024-02-05 04:06:32 +0900

    feat : 404 에러페이지 추가

--- src/main/java/org/example/basic/domain/account/payload/request/SaveAccountRequest.java
+++ src/main/java/org/example/basic/domain/account/payload/request/SaveAccountRequest.java
@@ -1,5 +1,7 @@
 package org.example.basic.domain.account.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,13 @@ import org.example.basic.domain.account.constant.Role;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SaveAccountRequest {
+    @NotBlank(message = "이름을 입력해주세요.")
     private String name;
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String loginId;
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
+    @NotNull(message = "권한을 선택해주세요.")
     private Role role;
 
     public void encodePassword(String password) {
